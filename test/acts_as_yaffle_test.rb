9d1afd7150dcadd700b2590b15e324e586d97516
Mike Mulvaney <mike.mulvaney@gmail.com>
2012-03-16 13:21:19 -0400

    switched test to make it pass -- had to make it check a symbol and not a string

--- test/acts_as_yaffle_test.rb
+++ test/acts_as_yaffle_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class ActsAsYaffleTest < Test::Unit::TestCase
   def test_a_hickwalls_yaffle_test_field_should_be_last_squawk
-    assert_equal "last_squawk", Hickwall.yaffle_text_field
+    assert_equal :last_squawk, Hickwall.yaffle_text_field
   end
 
   def test_a_wickwalls_yaffle_test_field_should_be_last_squawk
-    assert_equal "last_tweet", Hickwall.yaffle_text_field
+    assert_equal :last_tweet, Wickwall.yaffle_text_field
   end
 end
