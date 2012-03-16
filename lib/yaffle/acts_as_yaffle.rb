601b4d01811e6da035ff6d4f6ea182d0868773cd
Mike Mulvaney <mike.mulvaney@gmail.com>
2012-03-16 13:20:32 -0400

    building up through stage 4.1, but got error

---
+++ lib/yaffle/acts_as_yaffle.rb
@@ -0,0 +1,17 @@
+module Yaffle
+  module ActsAsYaffle
+    extend ActiveSupport::Concern
+
+    included do
+    end
+
+    module ClassMethods
+      def acts_as_yaffle(options = {})
+        cattr_accessor :yaffle_text_field
+        self.yaffle_text_field = (options[:yaffle_text_field] || :last_squawk)
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send :include, Yaffle::ActsAsYaffle
