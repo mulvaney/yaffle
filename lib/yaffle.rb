164fda2285e53f2af176f8940f3519041c65f10d
Mike Mulvaney <mike.mulvaney@gmail.com>
2012-03-16 13:11:52 -0400

    fresh from rails plugin new yaffle

---
+++ lib/yaffle.rb
@@ -0,0 +1,2 @@
+module Yaffle
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '76e6394b6a8cfea583c8938e9a61c7beac0506375b71595e343f1b530eb11ed9190d5f97fd6cbe5e7c5a6649f4c67b04b58cd1816116b4e89596843b4bea199c'
